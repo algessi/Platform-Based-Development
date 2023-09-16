dfd32dee402065b4e3d708e53ae23b7907f1444d
RedDotz20 <carlos_tabangay@outlook.com>
2023-09-16 14:24:09 +0800

    feat: calculator display | functions

--- script.js
+++ script.js
@@ -1,106 +1,132 @@
-const navbarMenu = document.querySelector(".navbar .links");
-const hamburgerBtn = document.querySelector(".hamburger-btn");
-const hideMenuBtn = navbarMenu.querySelector(".close-btn");
-const showPopupBtn = document.querySelector(".login-btn");
-const formPopup = document.querySelector(".form-popup");
-const hidePopupBtn = formPopup.querySelector(".close-btn");
-const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+const navbarMenu = document.querySelector('.navbar .links');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const hideMenuBtn = navbarMenu.querySelector('.close-btn');
+const showPopupBtn = document.querySelector('.login-btn');
+const formPopup = document.querySelector('.form-popup');
+const hidePopupBtn = formPopup.querySelector('.close-btn');
+const signupLoginLink = formPopup.querySelectorAll('.bottom-link a');
 
 // Show mobile menu
-hamburgerBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show-menu");
+hamburgerBtn.addEventListener('click', () => {
+	navbarMenu.classList.toggle('show-menu');
 });
 
 // Hide mobile menu
-hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+hideMenuBtn.addEventListener('click', () => hamburgerBtn.click());
 
 // Show login popup
-showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
+showPopupBtn.addEventListener('click', () => {
+	document.body.classList.toggle('show-popup');
 });
 
 // Hide login popup
-hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+hidePopupBtn.addEventListener('click', () => showPopupBtn.click());
 
 // Show or hide signup form
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
-    });
+signupLoginLink.forEach((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove'](
+			'show-signup'
+		);
+	});
 });
-
 
 // calculator.js
 
-let currentInput = '';
-let result = 0;
-let operator = '';
+const display = document.getElementById('display');
 
-function appendToResult(value) {
-  currentInput += value;
-  document.getElementById('result').value = currentInput;
-}
+const appendToResult = (value) => (display.value += value);
+const clearDisplay = () => (display.value = '');
 
-function clearResult() {
-  currentInput = '';
-  result = 0;
-  operator = '';
-  document.getElementById('result').value = '';
-}
+const calculateResult = () => {
+	try {
+		//? eval method to calculate displayed input
+		const result = eval(display.value);
 
-function calculateResult() {
-  // Validate the input.
-  if (!currentInput || isNaN(parseFloat(currentInput))) {
-    alert('Invalid input. Please enter a number.');
-    return;
-  }
+		//? return result value
+		display.value = result;
+	} catch (error) {
+		display.value = 'Math Error';
+	}
+};
 
-  // Perform the calculation.
-  switch (operator) {
-    case '+':
-      result += parseFloat(currentInput);
-      break;
-    case '-':
-      result -= parseFloat(currentInput);
-      break;
-    case '*':
-      result *= parseFloat(currentInput);
-      break;
-    case '/':
-      result /= parseFloat(currentInput);
-      break;
-    default:
-      result = parseFloat(currentInput);
-  }
+const updateResult = () => {
+	try {
+		eval(display.value);
+	} catch (error) {
+		display.value = 'Math Error';
+	}
+};
 
-  // Update the displayed result.
-  document.getElementById('result').value = result;
+//!------------------- Old Functions ----------------------
 
-  // Reset the current input and operator.
-  currentInput = '';
-  operator = '';
-}
+// let currentInput = '';
+// let result = 0;
+// let operator = '';
 
-// Add a button click listener to each math operation button.
-document.getElementById('plus').addEventListener('click', () => {
-  operator = '+';
-  calculateResult();
-});
+// function appendToResult(value) {
+// 	currentInput += value;
+// 	document.getElementById('result').value = currentInput;
+// }
 
-document.getElementById('minus').addEventListener('click', () => {
-  operator = '-';
-  calculateResult();
-});
+// function clearResult() {
+// 	currentInput = '';
+// 	result = 0;
+// 	operator = '';
+// 	document.getElementById('result').value = '';
+// }
 
-document.getElementById('multiply').addEventListener('click', () => {
-  operator = '*';
-  calculateResult();
-});
+// function calculateResult() {
+// 	// Validate the input.
+// 	if (!currentInput || isNaN(parseFloat(currentInput))) {
+// 		alert('Invalid input. Please enter a number.');
+// 		return;
+// 	}
 
-document.getElementById('divide').addEventListener('click', () => {
-  operator = '/';
-  calculateResult();
-});
+// 	// Perform the calculation.
+// 	switch (operator) {
+// 		case '+':
+// 			result += parseFloat(currentInput);
+// 			break;
+// 		case '-':
+// 			result -= parseFloat(currentInput);
+// 			break;
+// 		case '*':
+// 			result *= parseFloat(currentInput);
+// 			break;
+// 		case '/':
+// 			result /= parseFloat(currentInput);
+// 			break;
+// 		default:
+// 			result = parseFloat(currentInput);
+// 	}
 
+// 	// Update the displayed result.
+// 	document.getElementById('result').value = result;
 
+// 	// Reset the current input and operator.
+// 	currentInput = '';
+// 	operator = '';
+// }
+
+// // Add a button click listener to each math operation button.
+// document.getElementById('plus').addEventListener('click', () => {
+// 	operator = '+';
+// 	calculateResult();
+// });
+
+// document.getElementById('minus').addEventListener('click', () => {
+// 	operator = '-';
+// 	calculateResult();
+// });
+
+// document.getElementById('multiply').addEventListener('click', () => {
+// 	operator = '*';
+// 	calculateResult();
+// });
+
+// document.getElementById('divide').addEventListener('click', () => {
+// 	operator = '/';
+// 	calculateResult();
+// });
